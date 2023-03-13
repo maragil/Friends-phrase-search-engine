9494540589f5f7be3d87c868ea1554bbd1a6216d
Maria <araujomaria84@gmail.com>
2023-03-13 09:18:34 +0100

    All quotes filter

--- src/components/App.js
+++ src/components/App.js
@@ -8,11 +8,16 @@ import { useState } from 'react';
 function App() {
   /* VARIABLES ESTADO (DATOS) */
   const [allData, setAllData] = useState(phrases);
+  const [searchQuote, setSearchQuote] = useState('');
+  const [filterCharacter, setFilterCharacter] = useState('all');
   /* FUNCIONES HANDLER */
-
+  const handleFilterQuote = (ev) => {
+    setSearchQuote(ev.target.value);
+  }
   /* FUNCIONES Y VARIABLES AUXILIARES PARA PINTAR EL HTML */
   const renderList = () => {
     return allData
+    .filter((eachQuote) => eachQuote.quote.toLocaleLowerCase().includes(searchQuote.toLocaleLowerCase()))
     .map((eachQuote, index) => (
     <li key={index} className='li'>
       <p>Personaje: {eachQuote.character}</p>
@@ -29,6 +34,32 @@ function App() {
       </header>
       {/* main filter*/}
       <main>
+      <form>
+          <fieldset>
+            <legend>Filtrar por frase</legend>
+            <input
+              name="search"
+              placeholder="Filtrar por frase"
+              onInput={handleFilterQuote}
+              value={searchQuote}
+              />
+          </fieldset>
+          <fieldset>
+            <legend>Filtrar por personaje</legend>
+              <select
+                id="character"
+                onChange={handleFilterCharacter}
+                >
+                <option value="all">Todos</option>
+                <option value="Joey">Joey</option>
+                <option value="Rachel">Rachel</option>
+                <option value="Chandler">Chandler</option>
+                <option value="Phoebe">Phoebe</option>
+                <option value="Ross">Ross</option>
+                <option value="Monica">Monica</option>
+              </select>
+          </fieldset>
+        </form>
         {/*phrases list */}
         <section>
           <ul className='ul'>{renderList()}</ul>
